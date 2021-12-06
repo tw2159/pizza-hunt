b1959b55f9725881a1d6a70caa5c913a0dd8b01b
Te Wei <tw2159@nyu.edu>
2021-12-05 18:29:54 -0800

    update comment

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -3,28 +3,38 @@ const { Pizza } = require('../models');
 const pizzaController = {
     // get all pizzas
     getAllPizza(req, res) {
-      Pizza.find({})
-        .then(dbPizzaData => res.json(dbPizzaData))
-        .catch(err => {
-          console.log(err);
-          res.status(400).json(err);
-        });
-    },
+        Pizza.find({})
+          .populate({
+            path: 'comments',
+            select: '-__v'
+          })
+          .select('-__v')
+          .sort({ _id: -1 })
+          .then(dbPizzaData => res.json(dbPizzaData))
+          .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+          });
+      },
   
     // get one pizza by id
     getPizzaById({ params }, res) {
-      Pizza.findOne({ _id: params.id })
+        Pizza.findOne({ _id: params.id })
+        .populate({
+            path: 'comments',
+            select: '-__v'
+        })
+        .select('-__v')
         .then(dbPizzaData => {
-          // If no pizza is found, send 404
-          if (!dbPizzaData) {
+            if (!dbPizzaData) {
             res.status(404).json({ message: 'No pizza found with this id!' });
             return;
-          }
-          res.json(dbPizzaData);
+            }
+            res.json(dbPizzaData);
         })
         .catch(err => {
-          console.log(err);
-          res.status(400).json(err);
+            console.log(err);
+            res.status(400).json(err);
         });
     },
 
